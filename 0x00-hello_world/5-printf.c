bdce7c4821b44072440f3a6ec4eae7eed467506b
Fay-Keru <faithkerubo12@gmail.com>
2023-02-16 12:17:32 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - A c program using printf
+ * Descriprption:No using put function
+ * Return: 0 (success)
+ */
+
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
+
+
